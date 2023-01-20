5e7eb0317190d6d150e017294a53dab367226694
Nikita <zeninnikita96@gmail.com>
2023-01-20 23:05:37 +0100

    add dynamic routing

--- pages/users/users.js
+++ pages/users/users.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
@@ -12,12 +14,12 @@ function Users({ users }) {
     <div>
       <div>Users</div>
       {users.map((user) => (
-        <div key={user.id}>
+        <Link href={`/users/${user.id}`} key={user.id}>
           <div>
             <h3>{user.name}</h3>
             <p>{user.username}</p>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
