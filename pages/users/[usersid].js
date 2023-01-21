35b2e47445bd1d0bd49721213c094f05861e71e0
Nikita <zeninnikita96@gmail.com>
2023-01-21 12:09:43 +0100

    add getStaticPaths

--- pages/users/[usersid].js
+++ pages/users/[usersid].js
@@ -1,3 +1,20 @@
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  const arrOfPaths = data.map((user) => {
+    return {
+      params: {
+        id: user.id.toString(),
+      },
+    };
+  });
+  return {
+    paths: arrOfPaths,
+    fallback: false,
+  };
+};
+
 function Details() {
   return <div>Details Page</div>;
 }
